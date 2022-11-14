c52b8178ab2c5069d5878c12365f2d2a17ca3552
flexiblaze <ilikcay24@gmail.com>
2022-11-14 18:20:26 +0100

    practice 1 done

--- free_learning/js_linkedin/Practice/03_07/script.js
+++ free_learning/js_linkedin/Practice/03_07/script.js
@@ -7,3 +7,13 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+let telefoon = {
+    model: "Iphone 13 pro",
+    cost: "1200 euro",
+    opslag: "256 gb",
+    usb: "type c"
+}
+
+console.log(telefoon);
